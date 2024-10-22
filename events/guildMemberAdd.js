d9770cff2489213034632a90bedb2cfe6de50003
n0step_ <krushnavyas06@gmail.com>
2024-10-22 15:44:47 +0000

    fix: separator for username and mention

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,15 +5,15 @@ module.exports = {
   async execute(member) {
     try {
       const welcomeMessageRecord = await WelcomeMessage.findOne();
-
-      const welcomeMessage = welcomeMessageRecord ? welcomeMessageRecord.message : 'Welcome to the server, {username}!';
       
+      const welcomeMessage = welcomeMessageRecord ? welcomeMessageRecord.message : 'Welcome to the server, {username}!';
+
       const personalizedMessage = welcomeMessage.replace('{username}', member.user.username);
 
       const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome');
 
       if (welcomeChannel) {
-        welcomeChannel.send(`${personalizedMessage} <@${member.user.id}>`);
+        welcomeChannel.send(`${personalizedMessage} | <@${member.user.id}>`);
       } else {
         console.error('No channel named "welcome" found in the server.');
       }
